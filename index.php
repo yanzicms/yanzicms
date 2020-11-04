c65a29c7f4b09c4b941457fe482d096562d3e53a
yanzicms <804644245@qq.com>
2020-11-04 14:39:10 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Project: Yanzicms
+ * Producer: Yanzicms [ http://www.Yanzicms.com ]
+ * Author: A.J <804644245@qq.com>
+ * Copyright: http://www.Yanzicms.com All rights reserved.
+ */
+require __DIR__.'/app/swuuws/start.php';
